4837b15d5d74df7feec13be592ff16b1a098fc65
unclejee <swatxhim@outlook.com>
2024-05-07 03:33:04 +0800

    update title

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,3 +1,6 @@
+import { useRouter } from 'next/router'
+import { useConfig } from 'nextra-theme-docs'
+
 export default {
     logo: <span>🐨 Pykan</span>,
     faviconGlyph: '🐨',
@@ -9,6 +12,24 @@ export default {
       return {
         titleTemplate: '%s – Pykan'
       }
+    },
+    head: () => {
+      const { asPath, defaultLocale, locale } = useRouter()
+      const { frontMatter } = useConfig()
+      const url =
+        'https://pykan.org' +
+        (defaultLocale === locale ? asPath : `/${locale}${asPath}`)
+   
+      return (
+        <>
+          <meta property="og:url" content={url} />
+          <meta property="og:title" content={frontMatter.title || 'Pykan'} />
+          <meta
+            property="og:description"
+            content={frontMatter.description || 'Pykan'}
+          />
+        </>
+      )
     },
     footer: {
       text: (
